35e1f29c20a3cfb8b8b42f202604d7291904aa43
Alanghj <alancardoso122@hotmail.com>
2021-10-09 13:38:25 -0300

    The second Youtube_bot

--- youtube.py
+++ youtube.py
@@ -1,19 +1,46 @@
+from elements import pathies, click, click_2, click_3, click_4
+from youtube_cl import YouTube
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 
+MUSIC_NAME = 'Sad music'
 
-driver = webdriver.Chrome()
-driver.get("http://www.youtube.com/")
+def youtube_bot_path():
 
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys('sad music')
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/ytd-thumbnail/a/yt-img-shadow/img').click()
-time.sleep(8)
-driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button').click()
+    driver = webdriver.Chrome()
+    driver.get("http://www.youtube.com/")
 
-time.sleep(3600)
-driver.quit()
+    # Search the element
+
+    # path = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
+    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(MUSIC_NAME)
+
+    youtube = YouTube(pathies, click)
+
+    youtube.link_path(pathies)
+    youtube.click_path(click)
+
+    # # Click the search bar
+    time.sleep(2)
+    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
+    youtube.click_path(click_2)
+
+    # # Find the video
+    time.sleep(2)
+    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/ytd-thumbnail/a/yt-img-shadow/img').click()
+    youtube.click_path(click_3)
+    
+
+    # # Skip the video
+    time.sleep(8)
+    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button').click()
+    youtube.click_path(click_4)
+    
+
+    time.sleep(3600)
+    driver.quit()
+
+
+if __name__ == '__main__':
+    youtube_bot_path()
---
+++ youtube_cl.py
@@ -0,0 +1,17 @@
+# from youtube import driver
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+
+class YouTube:
+    def __init__(self, path, click):
+        self.path = path
+        self.click = click
+
+    def link_path(self, path):
+        return path
+
+
+    def click_path(self, click):
+        return click
+        
