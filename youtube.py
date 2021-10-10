9c885b4288d8a3b4803dea372cdb03283aa0b724
Alanghj <alancardoso122@hotmail.com>
2021-10-09 22:30:09 -0300

    The third youtube_bot

--- youtube.py
+++ youtube.py
@@ -1,8 +1,9 @@
-from elements import pathies, click, click_2, click_3, click_4
-from youtube_cl import YouTube
-from selenium import webdriver
-import time
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from youtube_cl import YouTube
+import pandas as pd
+import random
+import time
 
 MUSIC_NAME = 'Sad music'
 
@@ -13,29 +14,31 @@ def youtube_bot_path():
 
     # Search the element
 
-    # path = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
-    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(MUSIC_NAME)
+    path = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
+    click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(MUSIC_NAME)
 
-    youtube = YouTube(pathies, click)
+    youtube = YouTube(path, click)
 
-    youtube.link_path(pathies)
+    youtube.link_path(path)
     youtube.click_path(click)
 
     # # Click the search bar
     time.sleep(2)
-    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
-    youtube.click_path(click_2)
+    click = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
+    youtube.click_path(click)
 
     # # Find the video
+    dt = pd.read_json("youtube_list.json")
+    pathies = random.choice(dt['links'])
     time.sleep(2)
-    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/ytd-thumbnail/a/yt-img-shadow/img').click()
-    youtube.click_path(click_3)
+    click = driver.find_element_by_xpath(pathies).click()
+    youtube.click_path(click)
     
 
     # # Skip the video
-    time.sleep(8)
-    # click = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button').click()
-    youtube.click_path(click_4)
+    time.sleep(10)
+    click = driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button').click()
+    youtube.click_path(click)
     
 
     time.sleep(3600)
