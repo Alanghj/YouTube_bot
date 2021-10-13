82638c63f434d783072b2b0d817a9cf3b2a7c573
Alanghj <alancardoso122@hotmail.com>
2021-10-13 13:12:40 -0300

    4.2 The bot skip function

--- bot/youtube.py
+++ bot/youtube.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 import random
 import json
@@ -28,7 +30,7 @@ def youtube_bot_path():
 
     # File locations (../data_base/youtube_list.json) / data_base/youtube_list.json
     try:
-        with open('data_base/youtube_list.json') as f:
+        with open('data_base/youtube_list1.json') as f:
             data = json.load(f)
     except ImportError:
         print('The element json was not found!')
@@ -39,9 +41,15 @@ def youtube_bot_path():
     find_the_video.click()
 
     # Skip the video
-    skip_video = browser.find_element_by_xpath(
-        '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button')
-    skip_video.click()
+    ads_element = EC.visibility_of_element_located(
+        (By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button"))
+    for _ in range(0, 1):
+        if not ads_element:
+            break
+        else:
+            skip_video = browser.find_element_by_xpath(
+                '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button')
+            skip_video.click()
 
 
 if __name__ == '__main__':
